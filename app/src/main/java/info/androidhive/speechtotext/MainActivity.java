dbd557ec3e610ad073b6f5093139194df408632c
Tony Trinh <Tony.Trinh@monash.edu>
2017-04-20 22:47:24 +1000

    initial

---
+++ app/src/main/java/info/androidhive/speechtotext/MainActivity.java
@@ -0,0 +1,111 @@
+package info.androidhive.speechtotext;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	private TextView txtSpeechInput;
+	private ImageButton btnSpeak;
+	private final int REQ_CODE_SPEECH_INPUT = 100;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		txtSpeechInput = (TextView) findViewById(R.id.txtSpeechInput);
+		btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+
+		// hide the action bar
+		getActionBar().hide();
+
+		btnSpeak.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				promptSpeechInput();
+			}
+		});
+
+	}
+
+	/**
+	 * Showing google speech input dialog
+	 * */
+	private void promptSpeechInput() {
+		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+				RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+		intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+				getString(R.string.speech_prompt));
+		try {
+			startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+		} catch (ActivityNotFoundException a) {
+			Toast.makeText(getApplicationContext(),
+					getString(R.string.speech_not_supported),
+					Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	/**
+	 * Receiving speech input
+	 * */
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		switch (requestCode) {
+		case REQ_CODE_SPEECH_INPUT: {
+			if (resultCode == RESULT_OK && null != data) {
+
+				ArrayList<String> result = data
+						.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+				/*Toast toast = Toast.makeText(getApplicationContext(),result.get(0),Toast.LENGTH_LONG);
+				toast.setGravity(Gravity.TOP| Gravity.CENTER_VERTICAL, 5, 0);
+				toast.show();*/
+				//txtSpeechInput.setText(result.get(0));
+
+				LayoutInflater inflater = getLayoutInflater();
+				View layout = inflater.inflate(R.layout.custom_toast,
+						(ViewGroup) findViewById(R.id.custom_toast_container));
+
+				TextView text = (TextView) layout.findViewById(R.id.text);
+				text.setText(result.get(0));
+
+				Toast toast = new Toast(getApplicationContext());
+				toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+				toast.setDuration(Toast.LENGTH_LONG);
+				toast.setView(layout);
+				toast.show();
+			}
+			break;
+		}
+
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
